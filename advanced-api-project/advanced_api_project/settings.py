e674e73fb8ebc8d213fa7488671c38dfacb056ba
Oliver-coder-long <olishema11@gmail.com>
2025-08-11 17:03:35 +0300

    things on filters and search

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'django_filters.rest_framework.permissions.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+}
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,33 +2,59 @@ from django.shortcuts import render
 from .models import Author, Book
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from .filters import BookFilter
 
 class BookListView(ListView):
     model = Book
     template_name = 'book_list.html'
     context_object_name = 'books'
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['author', 'publication_year']
+    ordering = ['title']
 
 class BookDetailView(DetailView):
     model = Book
     template_name = 'book_detail.html'
     context_object_name = 'book'
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['author', 'publication_year']
+    ordering = ['title']
 
 class BookCreateView(CreateView):
     model = Book
     fields = ['title', 'publication_year', 'author']
     template_name = 'book_form.html'
     success_url = reverse_lazy('book-list') # redirect after creation
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['author', 'publication_year']
+    ordering = ['title']
 
 class BookUpdateView(UpdateView):
     model = Book
     fields = ['title', 'publication_year', 'author']
     template_name = 'book_form.html'
     success_url = reverse_lazy('book-list')
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['author', 'publication_year']
+    ordering = ['title']
 
 class BookDeleteView(DeleteView):
     model = Book
     template_name = 'book_confirm_delete.html'
     success_url = reverse_lazy('book-list')
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = BookFilter
+    search_fields = ['title', 'author']
+    ordering_fields = ['author', 'publication_year']
+    ordering = ['title']
 
 from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
