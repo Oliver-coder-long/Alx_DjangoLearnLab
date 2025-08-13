cfba1e0b103630400ac94c3feeaf1338f0cee144
Oliver-coder-long <olishema11@gmail.com>
2025-08-13 12:21:39 +0300

    minor change on databaseconfiguration

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'root',
+        'PASSWORD': 'mysqlingoho1+',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
